fef2bc52984e392097954cae388f292a878596ad
SatishMiral <satishmiral01@gmail.com>
2024-10-20 12:26:40 +0530

    converted code from commonjs to module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const puppeteer = require('puppeteer');
-const express = require('express');
-const cors = require('cors');
+import puppeteer from 'puppeteer';
+import express from 'express';
+import cors from 'cors';
+
 const app = express();
 
 app.use(cors());  // Enable CORS for all requests
@@ -8,18 +9,14 @@ app.use(cors());  // Enable CORS for all requests
 // Add a route to accept Flipkart URL as a query parameter
 app.get('/start-puppeteer', async (req, res) => {
     try {
-        console.log("FlipKart URL: " + req.query.url);
+        // console.log("FlipKart URL: " + req.query.url);
         const flipkartUrl = req.query.url;  // Get the Flipkart URL from the query parameter
         
         if (!flipkartUrl) {
             return res.status(400).send('Flipkart URL is required.');
         }
 
-        // Puppeteer launch with headless mode and necessary arguments
-        const browser = await puppeteer.launch({
-            headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
-        });
+        const browser = await puppeteer.launch();
         const page = await browser.newPage();
 
         // Navigate to the Flipkart page using the dynamic URL
@@ -40,8 +37,8 @@ app.get('/start-puppeteer', async (req, res) => {
             return price ? price.innerText : 'Element not found';
         });
 
-        console.log('Extracted Price from Flipkart:', extractedPrice);
-        console.log('Extracted Text from Flipkart:', extractedText);
+        // console.log('Extracted Price from Flipkart:', extractedPrice);
+        // console.log('Extracted Text from Flipkart:', extractedText);
 
         // Navigate to Amazon and get search results based on the extracted text
         const amazonUrl = `https://www.amazon.in/s?k=${encodeURIComponent(extractedText)}`;
@@ -70,15 +67,13 @@ app.get('/start-puppeteer', async (req, res) => {
         await browser.close();
 
         // Send back the extracted data as a response
-        res.json({ extractedText, extractedPrice, results });
+        res.json({ results });
     } catch (error) {
         console.error("Error running Puppeteer:", error);
         res.status(500).send("Failed to run Puppeteer script.");
     }
 });
 
-// Use the process.env.PORT for dynamic port assignment on Render
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(5000, () => {
+    console.log('Server is running on http://localhost:5000');
 });
