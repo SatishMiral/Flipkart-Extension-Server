892a88882b31a2fce236f528e6cbd7da821ad304
SatishMiral <satishmiral01@gmail.com>
2024-10-21 09:11:25 +0530

    Updated args and executable path for Puppeteer Launch

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ app.get('/start-puppeteer', async (req, res) => {
                 "--no-sandbox",
             ],
             headless: true,
-            // executablePath: process.env.PUPPETEER_EXECUTABLE_PATH, // Ensure correct Chromium path in Docker
         });
         
         const page = await browser.newPage();
